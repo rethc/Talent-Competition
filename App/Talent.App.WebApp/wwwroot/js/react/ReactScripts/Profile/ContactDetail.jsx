b3acb68fd7c8282bc2369f4e3ed7032e84682180
rethc <reth@xtra.co.nz>
2020-11-08 20:48:56 +1300

    Add last name to the primary contact details. Allow users to edit company contact details by clicking on the edit button.

--- App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/Profile/ContactDetail.jsx
+++ App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/Profile/ContactDetail.jsx
@@ -10,6 +10,7 @@ export class IndividualDetailSection extends Component {
             Object.assign({}, props.details)
             : {
                 firstName: "",
+                lastName: "",
                 email: "",
                 phone: ""
             }
@@ -76,6 +77,18 @@ export class IndividualDetailSection extends Component {
                     placeholder="Enter your first name"
                     errorMessage="Please enter a valid first name"
                 />
+
+                <ChildSingleInput
+                    inputType="text"
+                    label="Last Name"
+                    name="lastName"
+                    value={this.state.newContact.lastName}
+                    controlFunc={this.handleChange}
+                    maxLength={80}
+                    placeholder="Enter your last name"
+                    errorMessage="Please enter a valid last name"
+                />
+
                 <ChildSingleInput
                     inputType="text"
                     label="Email address"
@@ -107,6 +120,7 @@ export class IndividualDetailSection extends Component {
     renderDisplay() {
 
         let firstName = this.props.details ? `${this.props.details.firstName}` : ""
+        let lastName = this.props.details ? `${this.props.details.lastName}` : ""
         let email = this.props.details ? this.props.details.email : ""
         let phone = this.props.details ? this.props.details.phone : ""
 
@@ -114,7 +128,7 @@ export class IndividualDetailSection extends Component {
             <div className='row'>
                 <div className="ui sixteen wide column">
                     <React.Fragment>
-                        <p>Name: {firstName}</p>
+                        <p>Name: {firstName} {lastName}</p>
                         <p>Email: {email}</p>
                         <p>Phone: {phone}</p>
                     </React.Fragment>
@@ -237,7 +251,7 @@ export class CompanyDetailSection extends Component {
         let companyName = this.props.details ? this.props.details.name : ""
         let email = this.props.details ? this.props.details.email : ""
         let phone = this.props.details ? this.props.details.phone : ""
-        let location = {city:'',country:''}
+        let location = { city: '', country: '' }
         if (this.props.details && this.props.details.location) {
             location = this.props.details.location
         }
@@ -251,7 +265,7 @@ export class CompanyDetailSection extends Component {
                         <p>Phone: {phone}</p>
                         <p> Location: {location.city}, {location.country}</p>
                     </React.Fragment>
-                    <button type="button" className="ui right floated teal button">Edit</button>
+                    <button type="button" className="ui right floated teal button" onClick={this.openEdit}>Edit</button>
                 </div>
             </div>
         )
