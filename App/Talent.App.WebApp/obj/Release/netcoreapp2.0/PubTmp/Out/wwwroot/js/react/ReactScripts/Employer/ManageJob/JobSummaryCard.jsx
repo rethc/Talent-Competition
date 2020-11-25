f5e9ef52e81aeb71a24306809cc62563255808d8
rethc <reth@xtra.co.nz>
2020-11-25 23:37:01 +1300

    Deployment

---
+++ App/Talent.App.WebApp/obj/Release/netcoreapp2.0/PubTmp/Out/wwwroot/js/react/ReactScripts/Employer/ManageJob/JobSummaryCard.jsx
@@ -0,0 +1,53 @@
+﻿import React from 'react';
+import Cookies from 'js-cookie';
+import { Popup, Card, Icon, Label, Button, Segment } from 'semantic-ui-react';
+import moment from 'moment';
+
+export class JobSummaryCard extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            job: this.props.job
+        }
+        this.selectJob = this.selectJob.bind(this)
+    }
+
+    selectJob(id) {
+        var cookies = Cookies.get('talentAuthToken');
+        //url: 'http://localhost:51689/listing/listing/closeJob',
+    }
+
+    render() {
+        return (
+            <Card>
+                <Card.Content>
+                    <Card.Header>{this.state.job.title}</Card.Header>
+                    <Label as='a' color='black' ribbon='right'>
+                        <Icon name='user' /> {this.state.job.noOfSuggestions}
+                    </Label>
+                    <Card.Meta>{this.state.job.location.city}, {this.state.job.location.country}</Card.Meta>
+                    <Card.Description>{this.state.job.summary}</Card.Description>
+                </Card.Content>
+                <Card.Content extra>
+                    <Label color='red' size='tiny'>
+                        Expired
+                    </Label>
+                    <Button.Group floated='right' compact size='mini'>
+                        <Button basic color='blue'>
+                            <Icon name='ban' />
+                            Close
+                        </Button>
+                        <Button basic color='blue'>
+                            <Icon name='edit' />
+                            Edit
+                        </Button>
+                        <Button basic color='blue'>
+                            <Icon name='copy' />
+                            Copy
+                        </Button>
+                    </Button.Group>
+                </Card.Content>
+            </Card>
+        )
+    }
+}
---
+++ App/Talent.App.WebApp/obj/Release/netcoreapp2.0/PubTmp/Out/wwwroot/js/react/ReactScripts/Employer/ManageJob/ManageJob.jsx
@@ -0,0 +1,182 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+import Cookies from 'js-cookie';
+import LoggedInBanner from '../../Layout/Banner/LoggedInBanner.jsx';
+import { LoggedInNavigation } from '../../Layout/LoggedInNavigation.jsx';
+import { JobSummaryCard } from './JobSummaryCard.jsx';
+import { BodyWrapper, loaderData } from '../../Layout/BodyWrapper.jsx';
+import { Pagination, Icon, Dropdown, Checkbox, Accordion, Form, Segment, Header, Card } from 'semantic-ui-react';
+
+export default class ManageJob extends React.Component {
+    constructor(props) {
+        super(props);
+        let loader = loaderData
+        loader.allowedUsers.push("Employer");
+        loader.allowedUsers.push("Recruiter");
+        //console.log(loader)
+        this.state = {
+            loadJobs: [],
+            loaderData: loader,
+            activePage: 1,
+            sortBy: {
+                date: "desc"
+            },
+            filter: {
+                showActive: true,
+                showClosed: false,
+                showDraft: true,
+                showExpired: true,
+                showUnexpired: true
+            },
+            totalPages: 1,
+            activeIndex: "",
+            jobsFound: true
+
+        }
+        this.loadData = this.loadData.bind(this);
+        this.init = this.init.bind(this);
+        this.loadNewData = this.loadNewData.bind(this);
+
+        //your functions go here
+        this.renderJobs = this.renderJobs.bind(this);
+        this.renderNoJobs = this.renderNoJobs.bind(this);
+    };
+
+    init() {
+        let loaderData = TalentUtil.deepCopy(this.state.loaderData)
+        loaderData.isLoading = false;
+        this.setState({ loaderData });//comment this
+
+        //set loaderData.isLoading to false after getting data
+        this.loadData(() =>
+            this.setState({ loaderData })
+        );
+
+        //console.log(this.state.loaderData)
+    }
+
+    componentDidMount() {
+        this.init();
+    };
+
+    loadData(callback) {
+        var link = 'http://localhost:51689/listing/listing/getSortedEmployerJobs';
+        var cookies = Cookies.get('talentAuthToken');
+        // your ajax call and other logic goes here
+
+        $.ajax({
+            url: link,
+            headers: {
+                'Authorization': 'Bearer ' + cookies,
+                'Content-Type': 'application/json'
+            },
+            data: {
+                activePage: this.state.activePage,
+                sortbyDate: this.state.sortBy.date,
+                showActive: this.state.filter.showActive,
+                showClosed: this.state.filter.showClosed,
+                showDraft: this.state.filter.showDraft,
+                showExpired: this.state.filter.showExpired,
+                showUnexpired: this.state.filter.showUnexpired,
+            },
+            type: "GET",
+            success: function (res) {
+                console.log("Jobs: ", res);
+                this.setState({ loadJobs: res.myJobs })
+            }.bind(this),
+            error: function (res) {
+                console.log(res.status);
+            }
+        })
+    }
+
+    loadNewData(data) {
+        var loader = this.state.loaderData;
+        loader.isLoading = true;
+        data[loaderData] = loader;
+        this.setState(data, () => {
+            this.loadData(() => {
+                loader.isLoading = false;
+                this.setState({
+                    loadData: loader
+                })
+            })
+        });
+    }
+
+    renderNoJobs() {
+        return <Segment basic>No Jobs Found</Segment>
+    }
+
+    renderJobs() {
+        return (<Segment basic>
+            <Card.Group itemsPerRow='3'>
+                {this.state.loadJobs.map(job => <JobSummaryCard key={job.id} job={job} />)}
+            </Card.Group>
+        </Segment>)
+    }
+
+
+    render() {
+        const filterOptions = [
+            {
+                key: 'Active',
+                text: 'Active',
+                value: 'Active'
+            },
+            {
+                key: 'Closed',
+                text: 'Closed',
+                value: 'Closed'
+            },
+            {
+                key: 'Draft',
+                text: 'Draft',
+                value: 'Draft'
+            },
+            {
+                key: 'Expired',
+                text: 'Expired',
+                value: 'Expired'
+            },
+            {
+                key: 'Unexpired',
+                text: 'Unexpired',
+                value: 'Unexpired'
+            }
+        ];
+
+        return (
+            <BodyWrapper reload={this.init} loaderData={this.state.loaderData}>
+                <section className="page-body">
+                    <div className="ui container">
+                        <Header as='h1'>List of Jobs</Header>
+
+                        <Icon name='filter' />Filter:
+                        <Dropdown
+                            text='Choose filter'
+                            inline
+                            options={filterOptions}
+                        />
+                        <Icon name='calendar alternate outline' />Sort by date:
+                        <Dropdown
+                            text='Newest first'
+                            inline
+                        >
+                        </Dropdown>
+
+
+                        {this.state.jobsFound ? this.renderJobs() : this.renderNoJobs()}
+
+                        <Segment basic textAlign='center'>
+                            <Pagination
+                                defaultActivePage={1}
+                                totalPages={1}
+                            />
+                        </Segment>
+                    </div>
+                </section>
+            </BodyWrapper>
+        )
+    }
+}
